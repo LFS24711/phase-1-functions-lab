126d391b74ad67c6f40c6c32f2971fc496eb62fe
Logan <loganfranklinsmith@hotmail.com>
2021-12-09 16:12:29 -0500

    commit tree ea12f28b96b901438b55ede3c45c9118a8202c80

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    const distance = 42 - start;
+    return Math.abs(distance)
+
+};
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * 264;
+};
+
+function distanceTravelledInFeet(start, destination){
+    const distance = start - destination;
+    return Math.abs(distance * 264)
+};
+
+function calculatesFarePrice(start, destination) {
+    let fare = 0;
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return fare;
+    }
+    else if (distance > 400 & distance < 2000) {
+    fare = (distance - 400) * .02
+    return fare;
+    }
+
+    else if (distance > 2500){
+        return 'cannot travel that far'
+    }
+
+    else{
+        return fare = 25;
+    }
+}
+
